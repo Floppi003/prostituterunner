76ed5aceb0a6846ef2e02b41a911cd978997c9ee
Florian Peinsold <f.peinsold@mobileonly.com>
2017-03-17 09:25:57 +0100

    - missing files committed

---
+++ Assets/Scripts/Fatty.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fatty : MonoBehaviour {
+
+	public float unitsPerSecond = 4.5f;
+	public float speedVariance = 2.0f;
+
+	private float actualSpeed;
+
+	// Use this for initialization
+	void Start () {
+		// create a random actual speed
+		float randomVariance = Random.Range(0.0f, this.speedVariance);
+		randomVariance = randomVariance - (this.speedVariance / 2.0f); // make range halfway negative, halfway positive
+		this.actualSpeed = this.unitsPerSecond + randomVariance;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (this.transform.forward * (-1) * this.actualSpeed * Time.deltaTime);
+	}
+}
